ea0e82f93a33f0d6c3120aebc86dccb3dbea2436
Kesha <keshavraheja277@gmail.com>
2026-06-07 13:19:31 +0530

    fix: secure appointments API with token_required to prevent request context crash

--- server/routes/appointment_routes.py
+++ server/routes/appointment_routes.py
@@ -1,33 +1,34 @@
 from flask import Blueprint
-
-from controllers.appointment_controller import (
-    get_today_appointments
-)
-
 from controllers.appointment_controller import (
     create_appointment,
     get_appointments,
     update_appointment_status,
-    delete_appointment
+    delete_appointment,
+    get_today_appointments,
 )
+from middleware.auth_middleware import token_required, role_required
 
 appointment_bp = Blueprint("appointment_bp", __name__)
 
-appointment_bp.route("/", methods=["POST"])(create_appointment)
+_ALL_STAFF = ["Doctor", "Receptionist", "Admin"]
+_MANAGERS  = ["Doctor", "Admin"]
 
-appointment_bp.route("/", methods=["GET"])(get_appointments)
+appointment_bp.route("/", methods=["POST"])(
+    token_required(role_required(_ALL_STAFF)(create_appointment))
+)
 
-appointment_bp.route(
-    "/<id>/status",
-    methods=["PUT"]
-)(update_appointment_status)
+appointment_bp.route("/", methods=["GET"])(
+    token_required(role_required(_ALL_STAFF)(get_appointments))
+)
 
-appointment_bp.route(
-    "/<id>",
-    methods=["DELETE"]
-)(delete_appointment)
+appointment_bp.route("/<id>/status", methods=["PUT"])(
+    token_required(role_required(_ALL_STAFF)(update_appointment_status))
+)
 
-appointment_bp.route(
-    "/today",
-    methods=["GET"]
-)(get_today_appointments)
+appointment_bp.route("/<id>", methods=["DELETE"])(
+    token_required(role_required(_MANAGERS)(delete_appointment))
+)
+
+appointment_bp.route("/today", methods=["GET"])(
+    token_required(role_required(_ALL_STAFF)(get_today_appointments))
+)
